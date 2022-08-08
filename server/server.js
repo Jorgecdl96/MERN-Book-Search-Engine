5970f698d1f766bc59c75aedf0f704b99fe568d0
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-08 16:27:30 -0600

    adding apolloserver function

--- server/server.js
+++ server/server.js
@@ -13,8 +13,6 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.applyMiddleware({app});
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -23,14 +21,19 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-});
+    db.once('open', () => {
+      app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
 
+};
 
+startApolloServer(typeDefs, resolvers);
