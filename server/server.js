849e600fdeee5667199852725d84d26a54dd3a98
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-10 18:40:26 -0600

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
@@ -21,6 +22,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
