f4523bfcb16892a484b6f32db9a607e9a4f39467
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-12 01:33:43 -0600

    creating public folder in server to deployment

--- server/server.js
+++ server/server.js
@@ -14,16 +14,17 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '/public/index.html')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 const startApolloServer = async (typeDefs, resolvers) => {
